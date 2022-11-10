336332e8b389254f18dd9d01bc9b1a1145c02c9f
fakehyun00 <tienkhung128@gmail.com>
2022-11-10 18:26:45 +0700

    bocuc

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,73 +1,35 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
-import 'package:heocondihoc/components/history.dart';
-import 'package:heocondihoc/components/play.dart';
-import 'package:heocondihoc/screens/profile.dart';
+import 'package:heocondihoc/models/item.dart';
 
-import 'package:heocondihoc/screens/menu.dart';
-
-
-class HomeSreen extends StatefulWidget {
-  const HomeSreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<HomeSreen> createState() => _HomeSreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeSreenState extends State<HomeSreen> with TickerProviderStateMixin {
-  late TabController _tabController;
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 5, vsync: this, initialIndex: 2);
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 231, 238, 244),
-      bottomNavigationBar: Container(
-        color: Colors.grey,
-        child: TabBar(
-          controller: _tabController,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.white70,
-          indicatorSize: TabBarIndicatorSize.tab,
-          indicatorPadding: EdgeInsets.all(5.0),
-          indicatorColor: Colors.blue,
-          tabs: const <Widget>[
-            Tab(
-              icon: Icon(Icons.person),
-            ),
-            Tab(icon: Icon(Icons.history)),
-            Tab(icon: Icon(Icons.home)),
-            Tab(icon: Icon(Icons.shop)),
-            Tab(
-              icon: Icon(Icons.menu),
-            )
-          ],
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
+    return Container(
+      padding: EdgeInsets.fromLTRB(5, 20, 5, 0),
+      child: Column(
         children: [
-          Center(
-            child: ProfileScreen(),
-          ),
-          Center(
-            child: History(),
-          ),
-          Center(
-            child: PlayScreen(),
-          ),
-          Center(
-            child: Text('abc3'),
-          ),
-          Center(
-            child: MenuScreen(),
-          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(children: [
+                BackButton(
+                  onPressed: () {},
+                )
+              ]),
+              Column(
+                children: [
+                  ItemBar(),
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
