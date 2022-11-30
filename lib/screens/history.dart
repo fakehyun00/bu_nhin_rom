746bf1382b1b99d7722ca6ddb1adcdf62d2fcc5d
NgHung111 <thienhungnguyen397@gmail.com>
2022-11-30 16:24:46 +0700

    Edit History

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:heocondihoc/models/color.dart';
+import 'package:heocondihoc/models/header_info.dart';
 import 'package:heocondihoc/models/padding_shop.dart';
 
 import '../models/item.dart';
@@ -16,148 +17,159 @@ class _HistoryState extends State<History> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: background,
-        child: ListView(children: [
-          Container(
-            padding: EdgeInsets.fromLTRB(5, 5, 5, 0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Container(
+            decoration: background,
+            child: Column(children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Column(
                   children: [
-                    Container(),
-                    Column(
-                      children: [
-                        ItemBar(),
-                      ],
-                    )
+                    ItemBar(),
+                    Gap(20),
+                    Text(
+                      'Lịch sử',
+                      style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: myColor),
+                    ),
+                    divider,
                   ],
-                )
-              ],
-            ),
-          ),
-          Column(
-            children: [
-              Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 0)),
-              Text(
-                'Lịch sử',
-                style: TextStyle(
-                    fontSize: 35, fontWeight: FontWeight.bold, color: myColor),
-              ),
-              divider,
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'Đến giữa thế kỉ XIX, quyền hành \nthực tế của Nhật Bản nằm trong \ntay lực lượng chính trị nào?',
-                  style: TextStyle(fontSize: 20),
                 ),
               ),
-              Gap(20),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'Đến giữa thế kỉ XIX, các nước tư \nbản phương Tây đã sư dụng \nchính sách hay biện pháp gì để \nép Nhật Bản phải"mở cửa"',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Gap(20),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'Ý nào không phản ánh đúng \ntình hình kinh tế của Nhật \nBản từ đầu thế kỉ XIX đến trước \nnăm 1868?',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Gap(20),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'Đến giữa thế kỉ XIX, quyền hành \nthực tế của Nhật Bản nằm trong \ntay lực lượng chính trị nào?',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Gap(20),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'Đến giữa thế kỉ XIX, quyền hành \nthực tế của Nhật Bản nằm trong \ntay lực lượng chính trị nào?',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Gap(20),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'Đến giữa thế kỉ XIX, quyền hành \nthực tế của Nhật Bản nằm trong \ntay lực lượng chính trị nào?',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Gap(20),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'Đến giữa thế kỉ XIX, quyền hành \nthực tế của Nhật Bản nằm trong \ntay lực lượng chính trị nào?',
-                  style: TextStyle(fontSize: 20),
-                ),
-              )
-            ],
-          )
-        ]),
-      ),
-    );
+              Expanded(
+                  child: ListView(
+                children: [
+                  Column(children: [
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Card(
+                          child: ListTile(
+                            leading: Icon(Icons.topic_outlined),
+                            title: Text('Ải 1: Gà con đi tiểu học',
+                                style: TextStyle(fontSize: 20)),
+                            subtitle: Text(
+                              'Điểm cao nhất:90',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        )),
+                    Gap(20),
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Card(
+                          child: ListTile(
+                            leading: Icon(Icons.topic_outlined),
+                            title: Text('Ải 1: Gà con đi tiểu học',
+                                style: TextStyle(fontSize: 20)),
+                            subtitle: Text(
+                              'Điểm cao nhất:90',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        )),
+                    Gap(20),
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Card(
+                          child: ListTile(
+                            leading: Icon(Icons.topic_outlined),
+                            title: Text('Ải 1: Gà con đi tiểu học',
+                                style: TextStyle(fontSize: 20)),
+                            subtitle: Text(
+                              'Điểm cao nhất:90',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        )),
+                    Gap(20),
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Card(
+                          child: ListTile(
+                            leading: Icon(Icons.topic_outlined),
+                            title: Text('Ải 1: Gà con đi tiểu học',
+                                style: TextStyle(fontSize: 20)),
+                            subtitle: Text(
+                              'Điểm cao nhất:90',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        )),
+                    Gap(20),
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Card(
+                          child: ListTile(
+                            leading: Icon(Icons.topic_outlined),
+                            title: Text('Ải 1: Gà con đi tiểu học',
+                                style: TextStyle(fontSize: 20)),
+                            subtitle: Text(
+                              'Điểm cao nhất:90',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        )),
+                    Gap(20),
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Card(
+                          child: ListTile(
+                            leading: Icon(Icons.topic_outlined),
+                            title: Text('Ải 1: Gà con đi tiểu học',
+                                style: TextStyle(fontSize: 20)),
+                            subtitle: Text(
+                              'Điểm cao nhất:90',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        )),
+                    Gap(20),
+                  ])
+                ],
+              )),
+            ])));
   }
 }
